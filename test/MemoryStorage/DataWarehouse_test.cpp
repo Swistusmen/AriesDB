dbf561cab5e144e1a612f9ec07528da847c519be
Swistusmen <michal76aw@gmail.com>
2022-01-18 21:30:35 +0100

    Added tests

---
+++ test/MemoryStorage/DataWarehouse_test.cpp
@@ -0,0 +1,40 @@
+#include "gtest/gtest.h"
+
+#include "../../source/SQLCommands/From.h"
+
+#include "../../source/SQLCommands/Where.h"
+#include "../../source/SQLCommands/Select.h"
+
+#include "../../source/MemoryStorage/DataWarehouse.cpp"
+
+#include <vector>
+#include <memory>
+#include <utility>
+
+TEST(Data_Warehoouse,Simple_query_select_from_where_one_table){
+    // mocking compiler output
+    auto res=std::make_unique<std::vector<std::unique_ptr<SQLCommand>>>();
+    
+    res->emplace_back(std::make_unique<From>());
+    res->back()->addArgument("Shops");
+    res->push_back(std::make_unique<Where>());
+    res->back()->addArgument("id");
+    res->back()->addArgument(">");
+    res->back()->addArgument("3");
+    res->push_back(std::make_unique<Select>()); 
+    res->back()->addArgument("category");
+    res->back()->addArgument("shop");
+    // mocking compiler output
+    
+    DataWarehouse db;
+    auto output=db.executeQuery(std::move(res));
+    std::vector<std::string> flatTable;
+    flatTable.insert(flatTable.end(),output->columns.begin(),output->columns.end());
+    std::for_each(output->rows.begin(),output->rows.end(),[&flatTable](auto row){
+            flatTable.insert(flatTable.end(),row.begin(),row.end());
+    });
+    std::vector<std::string> expected{"shop", "category", "NewYorker", "Fashion", "Biedronka", "Supermarket"};
+
+    ASSERT_EQ(expected, flatTable);
+
+}
